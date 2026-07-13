ccb4ceccefc3f56607ea078d80db37c1744a2b65
Miccah <dev@miccah.io>
2026-07-13 00:18:00 -0700

    Add flake files

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "miccah.io";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          hugo
+        ];
+      };
+    };
+}
